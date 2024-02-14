60fa0e8fca3fb3c9c3120238622cc925a6ed5870
IshfatAbrar <ishfat0001abrar@gmail.com>
2024-02-14 13:47:04 +0600

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/lotr-app/",
   define: {
     "process.env": {},
   },
