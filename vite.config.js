7eed3a9ceddb21f9d1875d8f70de56d4d4246a42
IshfatAbrar <ishfat0001abrar@gmail.com>
2024-02-14 14:04:44 +0600

    update 1.0

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/lotr-app/",
+  base: "/LOTR-app/",
   define: {
     "process.env": {},
   },
